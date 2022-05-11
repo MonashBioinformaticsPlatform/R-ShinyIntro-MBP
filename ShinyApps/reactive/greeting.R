5cb834a4f280d510230306551891ea914532ebda
Nitika Kandhari <nitika.kandhari@monash.edu>
2022-05-11 15:02:11 +1000

    edit reactivity

--- ShinyApps/reactive/greeting.R
+++ ShinyApps/reactive/greeting.R
@@ -1,17 +1,11 @@
 ui <- fluidPage(
     textInput("name", "What's your name?"),
-    textOutput("greeting"),
-    textOutput("reverse")
+    textOutput("greeting")
 )
 
 server <- function(input, output, session) {
     output$greeting <- renderText({
-        paste0("Hello ", input$name, "!")
-    })
-    
-    output$reverse <- renderText({
-        rev <- reverse(input$name)
-        paste0("The reverse of your name is: ",rev , "!")
+        paste0("Hello ", input$name, "!", "\n", "Have a nice day!")
     })
 }
 
